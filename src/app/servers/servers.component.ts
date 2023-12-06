955dca8c54092814729b0cf6e8d9efd3980675d8
Rupam <rupampal48@gmail.com>
2023-12-06 12:18:12 +0530

    108cm Using relative paths in prog navigation v135

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -7,12 +8,21 @@ import { ServersService } from './servers.service';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  public servers: {id: number, name: string, status: string}[] = [];
+  public servers: { id: number, name: string, status: string }[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  public onReload() {
+    this.router.navigate(['/servers']);
+
+    //how we can use relative paths in navigate methods. You define relative to what this path should be loaded the defaults to Root URL that might be the behaviour you r looking for.
+
+    // this.router.navigate(['servers'], { relativeTo: this.route })
+  }
 }
